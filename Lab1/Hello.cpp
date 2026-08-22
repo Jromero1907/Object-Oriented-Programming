90220ae2a02cafaee13fc8889f12764e864c0160
Jromero1907 <jromero6@cpp.edu>
2026-08-22 10:01:35 -0700

    Add Hello.cpp for Lab1

--- Lab1/Hello.cpp
+++ Lab1/Hello.cpp
@@ -1,1 +1,16 @@
+/***
+ * This Lab Requires students to be able to create a simple code
+ * Named Hello World where they must create and run a function to
+ * print out "Hellow World!" and "Hello World again!".
+***/
 
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    printf("Hellow World again!\n");
+    return 0;
+}
